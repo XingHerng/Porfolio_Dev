9f0cb10c1b1cf5a47c7c882f1961ec3842904730
XingHerng <24042185@myrp.edu.sg>
2026-02-04 07:17:01 +0800

    test

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ import multer from "multer";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
